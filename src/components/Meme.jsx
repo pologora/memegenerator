d7b3e8cbbbe4a4e25467a26b486c197e828e660e
Oleksandr <lysakova@yahoo.com>
2022-01-21 20:33:49 +0100

    delete image from alt

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -67,7 +67,7 @@ export default function Meme() {
             <div className="meme-container" style={{ display: meme.randomImageUrl ? 'block' : 'none' }} >
                 <h2 className="meme-container--text top">{meme.topText}</h2>
                 <h2 className="meme-container--text bottom">{meme.bottomText}</h2>
-                <img className='meme-container--image' src={meme.randomImageUrl} alt="some meme image" />
+                <img className='meme-container--image' src={meme.randomImageUrl} alt="some meme" />
             </div>
         </main>
     )
